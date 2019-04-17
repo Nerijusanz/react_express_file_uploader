400a3c7d92cb98c6690751e9fa6ce9d19a3d6b38
nerijus-ubuntu <nerijusanz@gmail.com>
2019-04-17 19:10:21 +0300

    components/Progress.js Progress component development

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -1,10 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Progress(props) {
-  return <div />;
+function Progress({ percentage }) {
+  return (
+    <div className="progress">
+      <div
+        className="progress-bar progress-bar-striped bg-success"
+        role="progressbar"
+        style={{ width: `${percentage}%` }}
+      >
+        {percentage}%
+      </div>
+    </div>
+  );
 }
 
-Progress.propTypes = {};
+Progress.propTypes = {
+  percentage: PropTypes.number.isRequired
+};
 
 export default Progress;
