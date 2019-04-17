b0bd1463b6aa1bcbbf8a58127d7ad5d54c82b6b1
nerijus-ubuntu <nerijusanz@gmail.com>
2019-04-17 19:02:40 +0300

    components/Message.js development

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-function Message(props) {
+function Message({ msg }) {
   return (
-    <div>
-      
+    <div className="alert alert-info alert-dismissible fade show" role="alert">
+      {msg}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
-  )
+  );
 }
 
 Message.propTypes = {
+  msg: PropTypes.string.isRequired
+};
 
-}
-
-export default Message
-
+export default Message;
