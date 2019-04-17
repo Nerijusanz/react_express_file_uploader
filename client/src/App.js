88cb287221991f0b801b55aa50dc48f5f79ddddd
nerijus-ubuntu <nerijusanz@gmail.com>
2019-04-17 13:22:43 +0300

    App.js component changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 
 const App = () => (
   <div className="conatiner mt-4">
-    <h2>File uploader</h2>
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react"></i>
+      <span>React File Upload</span>
+    </h4>
   </div>
 );
 
